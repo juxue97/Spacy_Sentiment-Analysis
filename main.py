79dc5ad9651429f599f7dca5904cc95acf381f85
Teh Hung Wei <122148337+juxue97@users.noreply.github.com>
2024-04-07 00:45:09 +0800

    file updation

---
+++ main.py
@@ -0,0 +1,47 @@
+import pickle
+from pathlib import Path
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from tokenizer import tokenizer,sentiment
+
+###########################################################################################################################
+# Load Trained Model
+
+version = '0.1.0'
+base_dir = Path(__file__).resolve(strict=True).parent
+
+model=pickle.load(open(f'{base_dir}/trained_pipe-{version}.pkl','rb'))
+
+##############################################################################################################################
+
+# API
+# Load webapp api
+app = FastAPI()
+
+class TextIn(BaseModel):
+    text: str
+
+class PredictionOut(BaseModel):
+    result: str
+
+@app.get("/")
+def home():
+    return {"health_check": "OK", "model_version": version}
+
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: TextIn):
+    tokenized = tokenizer(payload.text)
+    pred = model.predict([tokenized])
+    result = sentiment(pred)
+    return {"result": result}
+
+text = 'this movie is great!'
+text = tokenizer(text)
+print(text)
+if __name__ == '__main__':
+
+    result = model.predict([text])
+    #print(result)
+    print(sentiment(result))
---
+++ tokenizer.py
@@ -0,0 +1,25 @@
+import spacy
+import re
+import string
+from spacy.lang.en.stop_words import STOP_WORDS
+
+nlp = spacy.load('en_core_web_md')
+punctuations = string.punctuation
+
+def tokenizer(text):
+    # Remove <br /> tags, emojis
+    clean_text = re.sub('<br\s*/?>', '', text)
+    clean_text = re.sub(':\S+', '', clean_text)
+
+    doc = nlp(clean_text)
+    # lemmatize -> remove stopwords and punctuations
+    lemma_tokens = [token.lemma_ for token in doc if token.pos_ !='PRON']
+    clean_tokens = ' '.join([token.lower() for token in lemma_tokens if token not in STOP_WORDS and token not in punctuations])
+
+    return clean_tokens
+
+def sentiment(result):
+    if result > 0:
+        return "Positive"
+    else:
+        return "Negative"
